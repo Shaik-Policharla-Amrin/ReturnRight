7df64b8e46b821ebee3b81e30d0fd69023cead23
Shaix26 <policharlaamrin@gmail.com>
2026-09-20 12:12:12 +0530

    Fix production Cognito authentication

--- src/authConfig.ts
+++ src/authConfig.ts
@@ -1,8 +1,10 @@
 export const cognitoAuthConfig = {
   authority: 'https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_dTRMpysvm',
   client_id: '1d1hfq25uin2f3mha98o6vfk66',
-  redirect_uri: 'http://localhost:5173',
-  post_logout_redirect_uri: 'http://localhost:5173',
+  redirect_uri: 'https://main.dtjfzels7mmz4.amplifyapp.com/',
+  redirectSignIn: window.location.origin,
+  redirectSignOut: window.location.origin,
+  post_logout_redirect_uri: 'https://main.dtjfzels7mmz4.amplifyapp.com/',
   response_type: 'code',
   scope: 'phone openid email',
 }
